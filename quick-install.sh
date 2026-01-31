40ae6990bb4c459a1252a2ac197990d58999809f
duzda <25201406+duzda@users.noreply.github.com>
2026-01-31 08:05:32 +0100

    chore: shellcheck

--- quick-install.sh
+++ quick-install.sh
@@ -1,6 +1,7 @@
-#/bin/sh
+#!/bin/sh
 
-PWD=`pwd`
-cd `dirname $0`
-cp "./build/src/libddb_misc_filebrowser_reborn.so" "/home/$USER/.local/lib/deadbeef/ddb_misc_filebrowser_reborn.so"
-cd $PWD
+PWD=$(pwd)
+(
+    cd "$(dirname $0)" || exit 1
+    cp "./build/src/libddb_misc_filebrowser_reborn.so" "/home/$USER/.local/lib/deadbeef/ddb_misc_filebrowser_reborn.so"
+)
--- quick-remove.sh
+++ quick-remove.sh
@@ -1,3 +1,3 @@
-#/bin/sh
+#!/bin/sh
 
 rm "/home/$USER/.local/lib/deadbeef/ddb_misc_filebrowser_reborn.so"
